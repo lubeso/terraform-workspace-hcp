8d0fc6e6844e9db6ec1cd5541bb2e5d87b0550f8
Luis Becerra Solis <lubeso@icloud.com>
2026-01-26 00:28:01 -0500

    deps: add github terraform provider

--- providers.tf
+++ providers.tf
@@ -1,10 +1,18 @@
 terraform {
   required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.10.2"
+    }
     tfe = {
       source  = "hashicorp/tfe"
       version = "~> 0.73.0"
     }
   }
+}
+
+provider "github" {
+  # This block is purposely empty
 }
 
 provider "tfe" {
