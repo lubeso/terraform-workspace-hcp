914d0de97ce21b7bca8b3fcacdcd01eef3393691
Luis Becerra Solis <lubeso@icloud.com>
2026-01-25 22:40:35 -0500

    chore: add project data source

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 data "tfe_organization" "main" {
   # This block is purposely empty
 }
+
+data "tfe_project" "main" {
+  name         = var.project.name
+  organization = data.tfe_organization.main.name
+}
