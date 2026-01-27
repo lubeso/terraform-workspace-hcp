8d536877009999ab8161c602104ef43a817c02aa
Luis Becerra Solis <lubeso@icloud.com>
2026-01-27 02:00:47 -0500

    fix: use correct attribute

--- workspaces.tf
+++ workspaces.tf
@@ -6,5 +6,5 @@ module "workspaces" {
   source                              = "./modules/tfe/workspace"
   name                                = each.key
   project_id                          = data.tfe_project.main.id
-  vcs_repo_github_app_installation_id = data.tfe_github_app_installation.main.id
+  vcs_repo_github_app_installation_id = data.tfe_github_app_installation.main.installation_id
 }
