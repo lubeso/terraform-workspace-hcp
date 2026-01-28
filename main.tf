ef1023b62fa9e62b02bfed96189652fbc8a76e98
Luis Becerra Solis <lubeso@icloud.com>
2026-01-27 22:42:46 -0500

    chore: add workspaces

--- main.tf
+++ main.tf
@@ -13,3 +13,14 @@ data "tfe_workspace" "main" {
 data "tfe_github_app_installation" "main" {
   installation_id = var.github_app_installation_id
 }
+
+module "workspaces" {
+  for_each = {
+    for workspace_name in var.var.workspace_names
+    : workspace_name => true
+  }
+  source                              = "./modules/workspace"
+  name                                = each.key
+  project_id                          = data.tfe_project.main.id
+  vcs_repo_github_app_installation_id = data.tfe_github_app_installation.main.installation_id
+}
